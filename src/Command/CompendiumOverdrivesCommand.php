3577bc83ed340d3ca50dd11ff004d0a07fc6a2b6
Vivien De Bona <vdebona@umanit.fr>
2022-11-04 13:59:21 +0100

    chore: start overdrives compendium génération

---
+++ src/Command/CompendiumOverdrivesCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'app:compendium:overdrives',
+    description: 'Génération du compendium des overdrives',
+)]
+class CompendiumOverdrivesCommand extends AbstractCompendiumCommand
+{
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $items = [];
+
+        foreach ($this->getList() as $data) {
+            $apiData = $this->getItem($data['id']);
+            $itemData = $this->getBaseData();
+        }
+
+        try {
+            $this->dumpCompendium($items);
+
+            $io->success('Compendium généré.');
+        } catch (\Throwable $e) {
+            $io->error(sprintf('Erreur de génération du compendium : %s', $e->getMessage()));
+        }
+
+        return Command::SUCCESS;
+    }
+
+    protected function getType(): string
+    {
+        return 'overdrive';
+    }
+
+    protected function getEmptyObjectPaths(): array
+    {
+        return [];
+    }
+}
